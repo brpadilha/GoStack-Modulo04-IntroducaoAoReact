a782c2303b3b8579e37dcb35ea34da84b149d6dd
Brpadilha <brpadilha.dev@gmail.com>
2019-09-09 17:13:16 -0300

    Remove itens da lista

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -18,13 +18,22 @@ class TechList extends Component {
 			newTech: '',
 		});
 	};
+
+	handleDelete = tech => {
+		this.setState({ techs: this.state.techs.filter(t => t !== tech) });
+	};
 	render() {
 		return (
 			<>
 				<form onSubmit={this.handleSubmit}>
 					<ul>
 						{this.state.techs.map(tech => (
-							<li key={tech}>{tech}</li>
+							<li key={tech}>
+								{tech}
+								<button onClick={() => this.handleDelete(tech)} type="button">
+									Remover
+								</button>
+							</li>
 						))}
 					</ul>
 					<input
