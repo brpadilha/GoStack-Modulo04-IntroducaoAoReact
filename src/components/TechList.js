3580917de24fb9f656f7d12e7dbc5f5667dbc6d1
Brpadilha <brpadilha.dev@gmail.com>
2019-09-09 14:48:21 -0300

    Stados dos componentes

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,3 +1,19 @@
 import React, { Component } from 'react';
 
-class TechList {}
+class TechList extends Component {
+	state = {
+		techs: ['Node.js', 'ReactJS', 'ReactNative'],
+	};
+
+	render() {
+		return (
+			<ul>
+				<li>Node.js</li>
+				<li>ReactJS</li>
+				<li>React Native</li>
+			</ul>
+		);
+	}
+}
+
+export default TechList;
