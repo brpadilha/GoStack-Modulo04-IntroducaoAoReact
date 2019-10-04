ce53b9df09810ed90a1840ee5521c19e7937fc92
Brpadilha <brpadilha.dev@gmail.com>
2019-10-04 14:48:33 -0300

    Configura componente raiz

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-const soma = (a, b) => a + b;
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
 
-alert(soma(1, 6));
+render(<App />, document.getElementById('app'));
