33af724fed74331c8ec9f501149f138c44a7e6e6
Brpadilha <brpadilha.dev@gmail.com>
2019-10-07 19:10:02 -0300

    proptypes

--- src/components/TechItem.js
+++ src/components/TechItem.js
@@ -1,15 +1,23 @@
 import React from 'react';
-
-function TechItem({ tech, onDelete }) {
+import PropTypes from 'prop-types';
+function TechItem({ tech = ' ', onDelete }) {
 	return (
 		<li>
 			{tech}
 			<button onClick={onDelete} type="button">
-				{' '}
 				Remover
 			</button>
 		</li>
 	);
 }
+
+TechItem.defaultProps = {
+	tech: 'Oculto',
+};
+
+TechItem.PropTypes = {
+	tech: PropTypes.string,
+	onDelete: PropTypes.func.isRequired,
+};
 
 export default TechItem;
