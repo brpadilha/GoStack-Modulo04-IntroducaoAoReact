baab186d234c3afee9b32a306749c7bd7c5a6958
Brpadilha <brpadilha.dev@gmail.com>
2019-10-07 18:39:00 -0300

    Remove os itens listados

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,16 +2,48 @@ import React, { Component } from 'react';
 
 class TechList extends Component {
 	state = {
+		newTech: '',
 		techs: ['Node.js', 'React.js', 'React Native'],
 	};
+
+	handleInputChange = e => {
+		this.setState({ newTech: e.target.value });
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+		this.setState({
+			techs: [...this.state.techs, this.state.newTech],
+			newTech: '',
+		});
+	};
+
+	handleDelete = tech => {
+		this.setState({ techs: this.state.techs.filter(t => t !== tech) });
+	};
 	render() {
-		console.log(this.state);
 		return (
-			<ul>
-				<li>Node.js</li>
-				<li>React.JS</li>
-				<li>React Native</li>
-			</ul>
+			<>
+				<form onSubmit={this.handleSubmit}>
+					<ul>
+						{this.state.techs.map(tech => (
+							<li key={tech}>
+								{tech}
+								<button onClick={() => this.handleDelete(tech)} type="button">
+									{' '}
+									Remover
+								</button>
+							</li>
+						))}
+					</ul>
+					<input
+						type="text"
+						onChange={this.handleInputChange}
+						value={this.state.newTech}
+					/>
+					<button type="submit"> Enviar </button>
+				</form>
+			</>
 		);
 	}
 }
